be07439521e39d56642cfab657da8af4cd3692ac
225junior <225juniorbrindou@gmail.com>
2022-07-01 00:43:45 +0000

    delete react-bootstrap react-dnd  add dbJson

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-
-app.listen(port, () => {
-  console.log('Le serveur a démaré...     http:\\\\localhost:3000 ')
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
 })
